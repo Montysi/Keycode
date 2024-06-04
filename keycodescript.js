12b7bb5c22b9630022844a056e0e9dcaaafd667e
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-04 15:58:08 +0100

    fourth commit

--- keycodescript.js
+++ keycodescript.js
@@ -1,7 +1,7 @@
 const welcomeBox = document.getElementById('welcome');
 
 document.addEventListener('keypress', () => {
-    welcomeBox.style.display = 'none'
+    welcomeBox.style.visibility = 'hidden'
 });
 
 
@@ -22,3 +22,55 @@ document.addEventListener("keypress", (event) => {
   whichDisplay.textContent = `${event.which}`;
 });
 
+const container = welcomeBox.parentElement; 
+
+container.style.display = "flex";
+container.style.justifyContent = "center";
+container.style.alignItems = "center";
+container.style.height = "100vh"; 
+
+welcomeBox.style.border = "2px solid black";
+welcomeBox.style.display = "flex";
+welcomeBox.style.alignItems = "center";
+welcomeBox.style.justifyContent = "center";
+welcomeBox.style.width = "50%";
+welcomeBox.style.padding = "20px";
+
+
+const informationDisplay = document.getElementById("displayContainer");
+
+informationDisplay.style.display = "flex";
+informationDisplay.style.alignItems = "center";
+informationDisplay.style.justifyContent = "space-evenly";
+// informationDisplay.style.visibility = 'hidden'
+informationDisplay.style.aligncontent = "center";
+
+// welcomeBox.style.border = "2px solid black";
+// welcomeBox.style.display = "flex";
+// welcomeBox.style.alignItems = "center";
+// welcomeBox.style.justifyContent = "center";
+// welcomeBox.style.width = "50%";
+// welcomeBox.style.padding = "20px";
+
+
+
+
+
+
+document.addEventListener("keypress", () => {
+  informationDisplay.style.visibility = "";
+});
+
+
+const displayBoxCode = document.getElementById("displayCode")
+
+displayBoxCode.style.border = "2px solid black"
+// displayBoxCode.style.width = "10%"
+
+const displayBoxKey = document.getElementById("displayKey");
+
+displayBoxKey.style.border = "2px solid black";
+
+const displayBoxWhich = document.getElementById("displayWhich");
+
+displayBoxWhich.style.border = "2px solid black";
