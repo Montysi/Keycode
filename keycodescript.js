27dc8275bc664b03a5a4f507b795120251fe1f65
Sam Monaghan <170720936+Montysi@users.noreply.github.com>
2024-06-06 11:29:32 +0100

    CSS positioning and JS visibilty key press

--- keycodescript.js
+++ keycodescript.js
@@ -22,55 +22,7 @@ document.addEventListener("keypress", (event) => {
   whichDisplay.textContent = `${event.which}`;
 });
 
-const container = welcomeBox.parentElement; 
-
-container.style.display = "flex";
-container.style.justifyContent = "center";
-container.style.alignItems = "center";
-container.style.height = "100vh"; 
-
-welcomeBox.style.border = "2px solid black";
-welcomeBox.style.display = "flex";
-welcomeBox.style.alignItems = "center";
-welcomeBox.style.justifyContent = "center";
-welcomeBox.style.width = "50%";
-welcomeBox.style.padding = "20px";
-
-
-const informationDisplay = document.getElementById("displayContainer");
-
-informationDisplay.style.display = "flex";
-informationDisplay.style.alignItems = "center";
-informationDisplay.style.justifyContent = "space-evenly";
-// informationDisplay.style.visibility = 'hidden'
-informationDisplay.style.aligncontent = "center";
-
-// welcomeBox.style.border = "2px solid black";
-// welcomeBox.style.display = "flex";
-// welcomeBox.style.alignItems = "center";
-// welcomeBox.style.justifyContent = "center";
-// welcomeBox.style.width = "50%";
-// welcomeBox.style.padding = "20px";
-
-
-
-
-
-
-document.addEventListener("keypress", () => {
-  informationDisplay.style.visibility = "";
-});
-
-
-const displayBoxCode = document.getElementById("displayCode")
-
-displayBoxCode.style.border = "2px solid black"
-// displayBoxCode.style.width = "10%"
-
-const displayBoxKey = document.getElementById("displayKey");
-
-displayBoxKey.style.border = "2px solid black";
-
-const displayBoxWhich = document.getElementById("displayWhich");
-
-displayBoxWhich.style.border = "2px solid black";
+document.addEventListener("keypress", (event) => {
+  const displayContVis = document.getElementById("displayContainer");
+  displayContVis.style.visibility= "visible"
+})
